2c850d45aa91466cb6a9f7f7af5814b72481162e
Erik Nelson <enelson@woodstream.com>
2023-07-10 11:12:11 -0400

    Added sites to portfolio page.

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -48,6 +48,119 @@ const Portfolio = () => {
 
           </div>
 
+          <h2>Websites</h2>
+
+          <div className="website-wrap">
+            <div className="item">
+              <a href="https://www.dynatrap.com/" target="_blank">
+                <img src="/sites/dynatrap.jpg" alt="DynaTrap" width="500" height="244"/>
+                <div role="tooltip">
+                  <span className="project-title">DynaTrap</span>
+                  <span className="tech">www.dynatrap.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.perkypet.com/" target="_blank">
+                <img src="/sites/perkypet.jpg" alt="Perky-Pet" width="500" height="245"/>
+                <div role="tooltip">
+                  <span className="project-title">Perky-Pet</span>
+                  <span className="tech">www.perkypet.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.victorpest.com/" target="_blank">
+                <img src="/sites/victorpest.jpg" alt="Victor Pest" width="500" height="245"/>
+                <div role="tooltip">
+                  <span className="project-title">Victor</span>
+                  <span className="tech">www.victorpest.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.saferbrand.com/" target="_blank">
+                <img src="/sites/safer.jpg" alt="Safer" width="500" height="240"/>
+                <div role="tooltip">
+                  <span className="project-title">Safer</span>
+                  <span className="tech">www.saferbrand.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.zarebasystems.com/" target="_blank">
+                <img src="/sites/zareba.jpg" alt="Zareba Systems" width="500" height="244"/>
+                <div role="tooltip">
+                  <span className="project-title">Zareba Systems</span>
+                  <span className="tech">www.zarebasystems.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.mosquitomagnet.com/" target="_blank">
+                <img src="/sites/mosquitomagnet.jpg" alt="Mosquito Magnet" width="500" height="245"/>
+                <div role="tooltip">
+                  <span className="project-title">Mosquito Magnet</span>
+                  <span className="tech">www.mosquitomagnet.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.havahart.com/" target="_blank">
+                <img src="/sites/havahart.jpg" alt="Havahart" width="500" height="244"/>
+                <div role="tooltip">
+                  <span className="project-title">Havahart</span>
+                  <span className="tech">www.havahart.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.terro.com/" target="_blank">
+                <img src="/sites/terro.jpg" alt="TERRO" width="500" height="244"/>
+                <div role="tooltip">
+                  <span className="project-title">TERRO</span>
+                  <span className="tech">www.terro.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://vlink.victorpest.com/" target="_blank">
+                <img src="/sites/vlink.jpg" alt="VLINK" width="500" height="245"/>
+                <div role="tooltip">
+                  <span className="project-title">VLINK</span>
+                  <span className="tech">vlink.victorpest.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.woodstreambrands.ca/" target="_blank">
+                <img src="/sites/woodstreambrands.jpg" alt="Woodstream Brands Canada" width="500" height="232"/>
+                <div role="tooltip">
+                  <span className="project-title">Woodstream Brands Canada</span>
+                  <span className="tech">www.woodstreambrands.ca</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.woodstream.com/" target="_blank">
+                <img src="/sites/woodstream.jpg" alt="Woodstream Corporate" width="500" height="244"/>
+                <div role="tooltip">
+                  <span className="project-title">Woodstream Corporate</span>
+                  <span className="tech">www.woodstream.com</span>
+                </div>
+              </a>
+            </div>
+            <div className="item">
+              <a href="https://www.woodstreampartnerportal.com/" target="_blank">
+                <img src="/sites/woodstream-partner-portal.jpg" alt="Woodstream Partner Portal" width="500" height="244"/>
+                <div role="tooltip">
+                  <span className="project-title">Partner Portal</span>
+                  <span className="tech">woodstreampartnerportal.com</span>
+                </div>
+              </a>
+            </div>
+          </div>
+
         </section>
 
       </Standard>
